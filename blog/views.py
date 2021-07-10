280c9090650ab2a6190d479edae368a7a5302fd8
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2021-07-10 15:04:46 +0530

    nothing chaged

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Like, Profile, Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-<<<<<<< HEAD
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
-=======
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, CommentForm, SharedForm
->>>>>>> test
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView,UpdateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
