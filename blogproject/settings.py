8ebe5fd84da92a3aab26d89435b8862afd17404c
Ashutosh Pradhan <ashutoshpradhan164@gmail.com>
2022-03-01 15:18:25 +0530

    try to upload file on s3 bucket but failed

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -183,6 +183,22 @@ AWS_SECRET_ACCESS_KEY = '************************'
 AWS_STORAGE_BUCKET_NAME = '***********'
 
 
+# AWS_ACCESS_KEY_ID = 'AKIA4PJ4JNEXTEVSWU6F'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'nasaphoto'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+# AWS_DEFAULT_ACL = 'public-read'
+# AWS_LOCATION = 'static'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
 # AWS_S3_FILE_OVERWRITE = False
 # AWS_DEFAULT_ACL = None
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
