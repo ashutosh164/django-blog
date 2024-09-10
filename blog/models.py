1641a6e2753fb05e9e4b47083025c295970f64f7
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2024-09-10 23:28:12 +0530

    design change and default image set

--- blog/models.py
+++ blog/models.py
@@ -34,20 +34,20 @@ class Post(models.Model):
 
     # RESIZE THE IMAGE
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+    #
+    #     img = Image.open(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
 
 
 class Profile(models.Model):
     objects = models.Manager()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile')
+    image = models.ImageField(default='images.png', upload_to='profile')
     bio = models.CharField(max_length=200,null=True, blank=True)
     first_name = models.CharField(max_length=200, blank=True)
     last_name = models.CharField(max_length=200, blank=True)
@@ -71,14 +71,14 @@ class Profile(models.Model):
     #     ordering = ('-created',)
 
     # RESIZE THE IMAGE
-    def save(self,*args,**kwargs):
-        super(Profile, self).save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    # def save(self,*args,**kwargs):
+    #     super(Profile, self).save(*args, **kwargs)
+    #
+    #     img = Image.open(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300,300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
 
 
 LIKE_CHOICES = (
